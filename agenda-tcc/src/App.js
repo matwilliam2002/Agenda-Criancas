d654aadbce443b02d7143dcc971cfe6fa3b1c797
Mateus Cunha <mateuswrcunha@gmail.com>
2025-05-26 19:56:51 -0300

    Primeiro commit

---
+++ agenda-tcc/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Login from './Pages/login';
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ agenda-tcc/src/Pages/login.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Login(){
+    return(
+        <div className="card-login">  
+            <div className="card-login-left"> 
+                <div className="card-login-left-center">
+                    <h1> Login </h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
+
+<style>{`
+.card-login {
+ display: flex;
+`}</style>
