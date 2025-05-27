24854a6d8f3811ab335cbe5794a7529400729326
Mateus Cunha <mateuswrcunha@gmail.com>
2025-05-27 19:08:33 -0300

    Tela de login - Parte 1

--- agenda-tcc/src/Pages/login.js
+++ agenda-tcc/src/Pages/login.js
@@ -1,12 +1,24 @@
 import React from "react";
+import '../style/login.css';
 
 function Login(){
     return(
         <div className="card-login">  
             <div className="card-login-left"> 
                 <div className="card-login-left-center">
-                    <h1> Login </h1>
+                    <form className="form-login">
+                        <h1 className="titulo"> Login </h1>
+                        <label>Usuário</label>
+                        <input type="text" className="input-usuario" placeholder="Digite seu usuário"/>
+                        <label>Senha</label>
+                        <input type="password" className="input-senha" placeholder="Digite sua senha"/>
+                        <button type="submit" className="btn-login">Entrar</button>
+                        <button type="button" className="btn-cadastrar">Cadastrar</button>
+                    </form>
                 </div>
+            </div>
+            <div className="card-login-right">
+                
             </div>
         </div>
     );
@@ -14,7 +26,4 @@ function Login(){
 
 export default Login;
 
-<style>{`
-.card-login {
- display: flex;
-`}</style>
+
